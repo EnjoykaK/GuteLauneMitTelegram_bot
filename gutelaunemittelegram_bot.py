51d6c050be8bd4770988b26940ab1911439e72cf
EnjoykaK <olena.kozlitska@gmail.com>
2026-05-11 15:42:24 +0200

    add inline keyboard buttons

--- gutelaunemittelegram_bot.py
+++ gutelaunemittelegram_bot.py
@@ -1,8 +1,87 @@
+print("Bot started")
 from telegram import Update
+from telegram._utils.types import ReplyMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler
+import random
+
 TOKEN = "8788699632:AAECzdTbn-r7pesobmtN7hjSctVvfy1U6t8"
+
+async def button(update: Update, context:ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    if query.data == "joke":
+        jokes = ["Warun kann ein Fahrrad nicht umfallen? Da es zwei Räder hat! :)",
+        "Was ist orange und läuft durch den Wald? Eine Wanderine! :)",
+        "Warum können Geister so schlecht lügen? Wiel sie durchschaut werden! :)"
+        ]
+        await query.edit_message_text(random.choice(jokes))
+        
+    elif query.data == "compliment":
+        compliments = ["Du bist wie eine Sonnenschein! ;)", 
+                       "Du bist großartig! ;)",
+                       "Du bist ein seht guter Mensch! Weiss es! ;)"
+                       ]
+        await query.edit_message_text(random.choice(compliments))
+
+    elif query.data == "motivation":
+        motivations = ["Du schaffst das!", 
+                       "Gib nicht auf, du bist stärker als du denkst!", 
+                       "Jeder Tag ist eine neue Chance, um deine Ziele zu erreichen! Schritt für schritt kommst du voran!"
+                       ]
+        await query.edit_message_text(random.choice(motivations))
+
 async def start(update: Update, context: ContextTypes. DEFAULT_TYPE):
-    await update.message.reply_text("Hallo! Ich bin dein Bot für Gute Laune! :)")
+    keyboard = [
+        [InlineKeyboardButton("Witz", callback_data="joke")],
+        [InlineKeyboardButton("Kompliment", callback_data="compliment")],
+        [InlineKeyboardButton("Motivation", callback_data="motivation")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(
+            "Hallo! Ich bin dein Bot für Gute Laune! :)\n\n"
+            "Nütze die Befehle /joke, um einen Witz zu bekommen\n"
+            "Nütze den Befehl /compliment, um sich sicherer zu fühlen\n"
+            "Nütze den Befehl /motivation, um motiviert zu sein\n",
+            reply_markup=reply_markup
+        )
+
+async def joke (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    jokes = [
+        "Warun kann ein Fahrrad nicht umfallen? Da es zwei Räder hat! :)",
+        "Was ist orange und läuft durch den Wald? Eine Wanderine! :)",
+        "Warum können Geister so schlecht lügen? Wiel sie durchschaut werden! :)",
+    ]
+    await update.message.reply_text(random.choice(jokes))
+
+async def compliment (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    compliments = [
+        "Du bist wie eine Sonnenschein! ;)",
+        "Du bist großartig! ;)",
+        "Du bist ein seht guter Mensch! Weiss es! ;)"
+    ]
+    await update.message.reply_text(random.choice(compliments))
+
+async def motivation (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    motivations = [
+        "Du schaffst das!",
+        "Gib nicht auf, du bist stärker als du denkst!",
+        "Jeder Tag ist eine neue Chance, um deine Ziele zu erreichen! Schritt für schritt kommst du voran!"
+    ]
+    await update.message.reply_text(random.choice(motivations))
+
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-app.run_polling()
+app.add_handler(CommandHandler("joke", joke))
+app.add_handler(CommandHandler("compliment", compliment))
+app.add_handler(CommandHandler("motivation", motivation))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CallbackQueryHandler(button))
+print("Bot läuft")
+import asyncio
+asyncio.run(app.run_polling())
+
+
+
+
